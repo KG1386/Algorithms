8b077974ada1a69998678a8fbef6bd353e52c61b
zeiselt <kon837@gmail.com>
2016-08-08 21:43:32 +0300

    Optimisations

--- Naive_Bayes_text/Naive_Bayes_code.py
+++ Naive_Bayes_text/Naive_Bayes_code.py
@@ -1,30 +1,29 @@
-# Takes a number of text files pre-labelled as positive and negative to train the naive bayes algorithm. Upon extracting features, can predict
-# result of new files
-# based on http://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
+# Takes a number of text files pre-labelled as positive and negative to train the naive bayes algorithm.
+# Upon extracting features, can predict result of new files
+# based on:
+# http://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
 # http://blog.yhat.com/posts/naive-bayes-in-python.html
+# Algorithm with the current training set and functions performs better with disabled option of separating stop-words and no n-grams
 
-
+# Import custom files used with this program
 import wordFreqTable as wft
 import stringExtractor as se
-import math
-from nltk import PorterStemmer # Library that implements stemming functions
 
-pos_words = se.readFile("vocabulary", "voc_positive.txt")
-neg_words = se.readFile("vocabulary", "voc_negative.txt")
+# Contains linking words, currently unused
 linkages = se.readFile("vocabulary", "voc_linkages.txt")
 file_counter = 0
 vocab = {}
-cleaned_word_list = []
+
 test_word_list = []
 word_counts = {
-    "p": {},
-    "n": {}
+    "p": {},"n": {}
 }
 train_distribution = {
-    "p": 0.,
-    "n": 0.
+    "p": 0.,"n": 0.
 }
 categories = []
+
+
 # Read all the files in the folder
 all_text_files = se.readAllFiles("sample_training_set")
 # Having all the files in the folder saved as string, go through each
@@ -33,7 +32,7 @@ for text_file in all_text_files:
         category = "p"
     elif "neg" in text_file:
         category = "n"
-    file_counter = file_counter + 1
+    file_counter += 1
     print file_counter
     # Saves categories of files discovered as a list
     categories.append(category)
@@ -42,13 +41,8 @@ for text_file in all_text_files:
     # Call the function from other file imported to main file(here) to get frequency table for words
     counts = wft.create_freq_table(se.readFile("sample_training_set", str(text_file))) # Get the table with number of occurences from wordFreqTable
     words = wft.extr_words(se.readFile("sample_training_set", str(text_file))) # Get the individual words from the text
-    for word, count in counts.items():
-        # Make sure that file is longer that 3 letters, is not a stop word and not a digit
-        if len(word) >= 3 and word not in linkages and word.isdigit() is False:
-            # Transform the word into its basic form (increases recognition rate significantly)
-            word = PorterStemmer().stem_word(word)
-            # Add transformed word into list
-            cleaned_word_list.append(word)
+
+    cleaned_word_list = wft.obtainCleanedList(counts)
     # Assemble all words into pairs or more groupings(n-grams)
     n_grams = wft.find_bigrams(cleaned_word_list)
     # Get the table with number of occurences from wordFreqTable
@@ -65,51 +59,31 @@ for text_file in all_text_files:
             word_counts[category][word] += count
 
 # Obtain distribution of positive and negative reviews by finding percentage of each category
-a = sum(train_distribution.values())
 train_dist_positive = (train_distribution["p"] / sum(train_distribution.values())) # devision of positive number of files/total files
 train_dist_negative = (train_distribution["n"] / sum(train_distribution.values()))
 
+# Find all text files in specified folder
 all_text_files = se.readAllFiles("sample_test_set")
-# Having all the files in the folder saved as string, go through each
+# Reset counters for successful predictions and number of files
 suc_counter = 0.0
 file_counter = 0.0
-vocab_sum_pos = sum(word_counts["p"].values())
-vocab_sum_neg = sum(word_counts["n"].values())
-vocab_sum = sum(vocab.values())
 
+
+# Go over all test files in the folder
 for text_file in all_text_files:
-
-    log_prob_positive = 0.0 #
-    log_prob_negative = 0.0
-
     # Training data is collected, analyse given sample
     test_counts = wft.create_freq_table(se.readFile("sample_test_set", str(text_file)))  # Get the table with number of occurences from wordFreqTable
     words = wft.extr_words(se.readFile("sample_test_set", str(text_file)))  # Get the individual words from the text
-
-    for word, count in test_counts.items():
-        if len(word) >= 3 and word not in linkages and word.isdigit() is False:
-            word = PorterStemmer().stem_word(word) # Transform the word into its basic form
-            test_word_list.append(word) # Add transformed word into list
+    # Same function as for training
+    test_word_list = wft.obtainCleanedList(test_counts)
+    # Find n-grams
     test_n_grams = wft.find_bigrams(test_word_list)
+    # Obtain frequency table for text
     test_counts = wft.create_freq_table_simp(test_n_grams)  # Get the table with number of occurences from wordFreqTable
+    # Obtain probabilities for entire textfile for it being negative or positive
+    pos_score,neg_score = wft.obtainProbabilities(test_counts,vocab,word_counts)
 
-    for word, count in test_counts.items():
-        # Check if the word is in vocabulary at all, and calculate its score (how often it occurs)
-        if word in vocab:
-            p_word = vocab[word] / vocab_sum
-
-        # Find probability that word fits to positive or negative category
-        p_w_given_positive = word_counts["p"].get(word, 0.0) / vocab_sum_pos
-        p_w_given_negative = word_counts["n"].get(word, 0.0) / vocab_sum_neg
-        # Depending which of the words is larger, add it to probability log
-        if p_w_given_positive > 0:
-            log_prob_positive += math.log(count * p_w_given_positive / p_word)
-        if p_w_given_negative > 0:
-            log_prob_negative += math.log(count * p_w_given_negative / p_word)
-
-    # Goes from logspace to regular, prints filename and score for each file
-    pos_score = math.exp(log_prob_positive + math.log(train_dist_positive))
-    neg_score = math.exp(log_prob_negative + math.log(train_dist_negative))
+    # Find if prediction is correct and count how often its correct
     if pos_score > neg_score and "pos" in text_file:
         polarised_flag = True
         suc_counter += 1
@@ -120,12 +94,8 @@ for text_file in all_text_files:
         polarised_flag = False
     print "File -", text_file, ",scored positive -",pos_score, ",scored negative -", neg_score,"Flag -", polarised_flag
 
-    test_n_grams[:] = []
-    test_counts.clear()
-    words[:] = []
-    log_prob_positive, log_prob_negative = 0,0
-    test_word_list[:] = []
+    # Find how many files have been assessed
     file_counter += 1
 
-
+# Show how good the prediction was
 print "Successful estimations:",((suc_counter/file_counter)*100),"%"
--- Naive_Bayes_text/wordFreqTable.py
+++ Naive_Bayes_text/wordFreqTable.py
@@ -1,7 +1,9 @@
-# Module that takes file, extracts words from it, lowercases, removes punctuation and creates freqency table(how often word occurs in text)
-
+# Module that deals with text file processing such as: removing punctuation, lowercasing, counting occurences and creating n-grams
 import re
 import string
+from nltk import PorterStemmer # Library that implements stemming functions
+import math
+
 
 # Removes punctuation from the text
 def remove_punctuation(text):
@@ -28,11 +30,51 @@ def create_freq_table_simp(text):
         dict[word] = dict.get(word, 0.0) + 1.0
     return dict
 
-# Bigrams has been found to give the best performance
+# Unused at the moment
 def find_bigrams(input_list):
   bigram_list = []
   for i in range(len(input_list)-1):
       bigram_list.append(str(input_list[i]))
   return bigram_list
 
+# Function that checks if the word in a text file matches the requirements, converts it to basic form and records new word
+def obtainCleanedList (counts):
+    cleaned_word_list = []
+    for word, count in counts.items():
+        # Make sure that file is longer that 3 letters, is not a stop word and not a digit
+        if len(word) >= 3 and word.isdigit() is False:
+            # Transform the word into its basic form (increases recognition rate significantly)
+            word = PorterStemmer().stem_word(word)
+            # Add transformed word into list
+            cleaned_word_list.append(word)
+    return cleaned_word_list
 
+def obtainProbabilities(test_counts,vocab,word_counts):
+    log_prob_positive = 0.0  #
+    log_prob_negative = 0.0
+
+    pos_score, neg_score = 0,0
+    # Pre-calculate sums to save processing time
+    vocab_sum_pos = sum(word_counts["p"].values())
+    vocab_sum_neg = sum(word_counts["n"].values())
+    vocab_sum = sum(vocab.values())
+
+    for word, count in test_counts.items():
+        # Check if the word is in vocabulary at all, and calculate its score (how often it occurs)
+        if word in vocab:
+            p_word = vocab[word] / vocab_sum
+
+        # Find probability that word fits to positive or negative category
+        p_w_given_positive = word_counts["p"].get(word, 0.0) / vocab_sum_pos
+        p_w_given_negative = word_counts["n"].get(word, 0.0) / vocab_sum_neg
+
+        # Using logarithms allows to keep numbers manageable by python as log function scales them
+        if p_w_given_positive > 0:
+            log_prob_positive += math.log(count * p_w_given_positive / p_word)
+        if p_w_given_negative > 0:
+            log_prob_negative += math.log(count * p_w_given_negative / p_word)
+
+        # Exponent helps to get rid of negative numbers
+        pos_score = math.exp(log_prob_positive)
+        neg_score = math.exp(log_prob_negative)
+    return pos_score, neg_score
