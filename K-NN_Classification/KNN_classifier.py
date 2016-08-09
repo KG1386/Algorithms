251e9f31cac732cf1b5ef9790578dd27ab318b6f
zeiselt <kon837@gmail.com>
2016-08-09 17:07:08 +0300

    base code

---
+++ K-NN_Classification/KNN_classifier.py
@@ -0,0 +1,100 @@
+# Main file of the algorithm. Contains calls to other functions not included here
+
+# Example of kNN implemented from Scratch in Python
+
+import csv
+import random
+import math
+import operator
+
+# Normalises data before calculating distance
+def normalisation (training_set):
+    normalised_list = []
+    print range(len(training_set[0:(len(training_set[0])-1)]))
+    for x in range(len(training_set[0:(len(training_set[0])-1)])):
+        # print training_set[:][0:(len(training_set[x])-1)]
+        column = [item[x] for item in training_set]
+        min_val = min(column)
+        max_val = max(column)
+        for y in column:
+            print y
+            normalised = (y-min_val)/(max_val - min_val)
+            normalised_list.append(normalised)
+    return normalised_list
+
+
+def loadDataset(filename, split, trainingSet=[], testSet=[]):
+    with open(filename, 'rb') as csvfile:
+        lines = csv.reader(csvfile)
+        dataset = list(lines)
+        for x in range(len(dataset) - 1):
+            for y in range(4):
+                dataset[x][y] = float(dataset[x][y])
+            if random.random() < split:
+                trainingSet.append(dataset[x])
+            else:
+                testSet.append(dataset[x])
+
+
+def euclideanDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x] - instance2[x]), 2)
+    return math.sqrt(distance)
+
+
+def getNeighbors(trainingSet, testInstance, k):
+    distances = []
+    length = len(testInstance) - 1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == predictions[x]:
+            correct += 1
+    return (correct / float(len(testSet))) * 100.0
+
+
+def main():
+    # prepare data
+    trainingSet = []
+    testSet = []
+    split = 0.9
+    loadDataset('iris.csv', split, trainingSet, testSet)
+    #normalised_train_set = normalisation(trainingSet)
+    print 'Train set: ' + repr(len(trainingSet))
+    print 'Test set: ' + repr(len(testSet))
+    # generate predictions
+    predictions = []
+    k = 3
+    for x in range(len(trainingSet)):
+        neighbors = getNeighbors(trainingSet, testSet[x], k)
+        result = getResponse(neighbors)
+        predictions.append(result)
+        print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy) + '%')
+
+
+main()
---
+++ K-NN_Classification/README.txt
@@ -0,0 +1,5 @@
+Implementation of k-nearest neighbour classification without libraries. 
+KNN algorithm is a simple approach to classification that works by finding the distance from the test point to its neighbours within set boundary(k).
+
+Very effective for small datasets due to its speed and the fact that its "lazy", which means that it creates entire model only at the moment when the prediction is required. This helps to avoid any biases over the data
+and obtain localised model.
